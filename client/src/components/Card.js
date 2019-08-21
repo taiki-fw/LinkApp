b752420dbf639902ec8ab441855866bd1f10aed0
森　泰樹 <taiki@dot1x055.14-30-23.wlan.ms.dendai.ac.jp>
2019-08-21 15:58:31 +0900

    編集・閲覧状態の分岐を実装しました

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,14 +1,62 @@
 import React from "react";
 
-const Card = props => {
-  return (
-    <li>
-      <a href={props.url}>
-        <h3>{props.title}</h3>
-        <p>{props.comment}</p>
-      </a>
-    </li>
-  );
-};
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      comment: this.props.comment,
+      url: this.props.url,
+      completed: true
+    };
+  }
+
+  handleChange(e) {
+    const newValue = e.target.value;
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleClick() {}
+
+  render() {
+    return (
+      <>
+        {this.state.completed ? (
+          <li>
+            <a href={this.state.url}>
+              <h3>{this.state.title}</h3>
+              <p>{this.state.comment}</p>
+            </a>
+            <button>編集</button>
+          </li>
+        ) : (
+          <li>
+            <input
+              type="text"
+              value={this.state.title}
+              name="title"
+              onChange={e => this.handleChange(e)}
+            />
+            <input
+              type="text"
+              value={this.state.comment}
+              name="comment"
+              onChange={e => this.handleChange(e)}
+            />
+            <input
+              type="text"
+              value={this.state.url}
+              name="url"
+              onChange={e => this.handleChange(e)}
+            />
+            <button>完了</button>
+          </li>
+        )}
+      </>
+    );
+  }
+}
 
 export default Card;
