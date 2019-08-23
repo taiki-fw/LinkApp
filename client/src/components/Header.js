3bc22df40e5fea49f44e7e137698733ea143c645
森　泰樹 <taiki@mbp-2.local>
2019-08-24 00:21:18 +0900

    Headerの仮スタイルを当てました

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,14 +3,32 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
+    <header style={style.header}>
       <h1>
-        <Link to="/">LinkApp</Link>
+        <Link to="/" style={style.link}>
+          LinkApp
+        </Link>
       </h1>
-      <Link to="/post">新規投稿</Link>
-      <Link to="">ログアウト</Link>
-    </>
+      <Link to="/post" style={style.link}>
+        新規投稿
+      </Link>
+      {/* <Link to="">ログアウト</Link> */}
+    </header>
   );
+};
+
+const style = {
+  header: {
+    color: "#000",
+    backgroundColor: "#0D3F67",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1em"
+  },
+  link: {
+    color: "#fff"
+  }
 };
 
 export default Header;
