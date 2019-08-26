ad680c52ec6307b0ea139d273be6a9be2cc43a06
森　泰樹 <taiki@mbp-2.local>
2019-08-27 00:11:33 +0900

    TopPageにてログインしているかの分岐を追加

--- client/src/mainPage/TopPage.js
+++ client/src/mainPage/TopPage.js
@@ -1,19 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import request from "superagent";
 
 import CardList from "../components/CardList";
 
-const TopPage = () => {
-  return (
-    <>
-      <h1>タイトル</h1>
-      <Link to="/post" style={styles.Link}>
-        投稿
-      </Link>
-      <CardList />
-    </>
-  );
-};
+class TopPage extends React.Component {
+  componentWillMount() {
+    const sendObj = this.authUser();
+    if (sendObj.auth) {
+      window.alert(sendObj.msg);
+    } else {
+      this.props.history.push("/login");
+    }
+  }
+
+  authUser() {
+    request.get("/api/user/auth").end((err, obj) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      return obj;
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>タイトル</h1>
+        <Link to="/post" style={styles.Link}>
+          投稿
+        </Link>
+        <CardList />
+      </>
+    );
+  }
+}
 
 const styles = {
   Link: {
@@ -26,4 +48,4 @@ const styles = {
   }
 };
 
-export default TopPage;
+export default withRouter(TopPage);
