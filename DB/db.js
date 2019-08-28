7b39fbb2e6932c3dfa6f2027cd418525c9b7c27a
森　泰樹 <taiki@mbp-2.local>
2019-08-29 00:17:06 +0900

    [Add] localのpostgresとの接続の設定を実装
    testとして簡単なテーブルでテストを行った。
    次のコミットでUserとCardテーブルを作成していくと同時にリレーションの設定を行う

---
+++ DB/db.js
@@ -0,0 +1,27 @@
+const pg = require("pg");
+
+const connectionString = "postgres://taiki:80210880@localhost:5432/LinkAppNew";
+
+const pool = new pg.Pool({
+  connectionString: connectionString
+});
+
+// SELECT してみる
+pool
+  .query("SELECT * FROM test_table")
+  .then(result => {
+    console.log("Success", result);
+    // 結果データの表示
+    if (result.rows) {
+      result.rows.forEach((row, index) => {
+        console.log(index + 1, row);
+      });
+    }
+  })
+  .catch(error => {
+    console.log("Failure", error);
+  })
+  .then(() => {
+    console.log("切断");
+    pool.end();
+  });
