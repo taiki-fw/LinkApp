7c2e9420cb5c5be582fc269c4bbb5df2bfd3b8eb
森　泰樹 <taiki@mbp-2.local>
2019-09-30 23:10:07 +0900

    .envファイルでのコンテナ・DBの情報を置き換え

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,13 @@
 const pg = require("pg");
-
-// dotenvモジュールを利用して.envファイル無いの環境変数を読み込む
-require("dotenv").config();
-
-console.log(process.env.POSTGRE_HOST);
-console.log(process.env.POSTGRE_PORT);
-console.log(process.env.POSTGRE_DB_NAME);
-console.log(process.env.POSTGRE_USER);
-console.log(process.env.POSTGRE_PASSWORD);
+require("dotenv").config({ path: "../.env" });
+const env = process.env;
 
 const pool = new pg.Pool({
-  host: "localhost",
-  port: 5432,
-  database: "link",
-  user: "link",
-  password: "link"
+  host: env.POSTGRE_HOST,
+  port: env.POSTGRE_PORT,
+  database: env.POSTGRE_DB_NAME,
+  user: env.POSTGRE_USER,
+  password: env.POSTGRE_PASSWORD
 });
 
 module.exports = {
