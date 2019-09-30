d21e9a4a525d753a571fce638553535a32964f21
森　泰樹 <taiki@mbp-2.local>
2019-09-30 22:14:00 +0900

    .envファイルにPostgresの設定に使用する値を記述、dbファイルから参照を確認

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,14 @@
 const pg = require("pg");
 
+// dotenvモジュールを利用して.envファイル無いの環境変数を読み込む
+require("dotenv").config();
+
+console.log(process.env.POSTGRE_HOST);
+console.log(process.env.POSTGRE_PORT);
+console.log(process.env.POSTGRE_DB_NAME);
+console.log(process.env.POSTGRE_USER);
+console.log(process.env.POSTGRE_PASSWORD);
+
 const pool = new pg.Pool({
   host: "localhost",
   port: 5432,
