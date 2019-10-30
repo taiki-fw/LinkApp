52a703af901d320ba5496f4a534f8da789b2b9ef
taiki-fw <taiki_52@outlook.jp>
2019-10-30 19:18:59 +0900

    LinkCardを保存するactionとreducerの定義

---
+++ client/src/reducer/modules/linkCards.js
@@ -0,0 +1,70 @@
+import request from "superagent";
+
+let initialState = {
+  isFetching: false,
+  data: []
+};
+
+// action types
+const GET_LINK = "GET_LINK";
+const POST_LINK = "POST_LINK";
+const EDIT_LINK = "EDIT_LINK";
+const DELETE_LINK = "DELETE_LINK";
+
+function getLinkCard() {
+  return {
+    type: GET_LINK
+  };
+}
+
+function postLinkCardSuccess(linkData) {
+  return {
+    type: POST_LINK,
+    linkData
+  };
+}
+
+function editLinkCard(linkData) {
+  return {
+    type: EDIT_LINK,
+    linkData
+  };
+}
+
+function addLinkCard(linkDataId) {
+  return {
+    type: DELETE_LINK,
+    linkDataId
+  };
+}
+
+export function linkCards(state = initialState, action) {
+  switch (action.type) {
+    case POST_LINK:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: [...state.data, action.linkData]
+      });
+    case GET_LINK:
+      return Object.assign({}, state, {
+        isFetching: true,
+        data: []
+      });
+  }
+}
+
+export function addFetchLinkCard(linkData) {
+  return function(dispatch) {
+    dispatch(postLinkCardSuccess());
+    return request
+      .post("/api/createLink")
+      .send(linkData)
+      .end((err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        dispatch(postLinkCardSuccess(data));
+      });
+  };
+}
