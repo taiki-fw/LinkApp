9d3e751255b8669cad7fa7032d0f62bc930c4154
taiki-fw <taiki_52@outlook.jp>
2019-12-13 19:13:28 +0900

    feat: BookMarkを取得し表示するinput要素を実装

--- client/src/Container/BookMarks.js
+++ client/src/Container/BookMarks.js
@@ -1,22 +1,42 @@
-// import React from "react";
-const fs = require("fs");
+import React, { useState, useEffect } from "react";
 
-function folderDisassembly(obj) {
-  if (obj.type === "folder") obj.children.map(c => folderDisassembly(c));
-  else {
-    bookmarks.push({ name: obj.name, url: obj.url });
-  }
+// chromeのbookmarkが保存してあるmac内のファイルパス
+const bookmarkFilePath =
+  "/Users/taiki/Library/ApplicationSupport/Google/Chrome/Default/Bookmarks";
+
+export default function BookMarks() {
+  const [bookmarksArray, setBookmarks] = useState([]);
+
+  const getFileData = event => {
+    console.log(event.target.files);
+    let reader = new FileReader();
+    reader.readAsText(event.target.files[0]);
+    reader.onload = () => {
+      console.log(reader.result);
+      function folderDisassembly(obj) {
+        if (obj.type === "folder") obj.children.map(c => folderDisassembly(c));
+        else {
+          bookmarks.push({ name: obj.name, url: obj.url });
+        }
+      }
+      const bookmarksNode = JSON.parse(reader.result).roots.bookmark_bar
+        .children;
+      const bookmarks = [];
+      bookmarksNode.map(b => folderDisassembly(b));
+      setBookmarks(bookmarks);
+    };
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={e => getFileData(e)} />
+      <ul>
+        {bookmarksArray.map((obj, index) => (
+          <li key={index}>
+            <a href={obj.url}>{obj.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-const bookmarkFile = fs.readFileSync(
-  "/Users/taiki/Library/ApplicationSupport/Google/Chrome/Default/Bookmarks"
-); // chromeのbookmarkが保存してあるmac内のファイルパス
-const bookmarksNode = JSON.parse(bookmarkFile).roots.bookmark_bar.children;
-const bookmarks = [];
-bookmarksNode.map(b => folderDisassembly(b));
-console.log(bookmarks);
-
-// export default function BookMarks() {
-
-//   return <h1>Hello BookMarks</h1>;
-// }
